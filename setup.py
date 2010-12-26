0931509040fb5bcdf206bb37605617de5f421138
Gregg Lind <gregg.lind@gmail.com>
2010-12-26 16:32:26 -0600

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='duck',
+      version='0.1',
+      description='',
+      author='Gregg Lind',
+      author_email='gregg.lind@gmail.com',
+      url='https://github.com/gregglind/duck',
+      license='bsd',
+      packages=['duck',],
+     )
